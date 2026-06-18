71b0e9974038c4a15ff2fc353cc91c3a17cccc19
fgwsz <3191000458@qq.com>
2026-06-19 05:59:31 +0800

    导入语言转字幕工具

---
+++ qwen_asr_srt_install.sh
@@ -0,0 +1,10 @@
+# Qwen3-ASR-1.7B
+pip install modelscope
+modelscope download --model Qwen/Qwen3-ASR-1.7B --local_dir ~/Downloads/Qwen3-ASR-1.7B
+pip install qwen-asr
+# aeneas
+sudo apt update
+sudo apt install libespeak-dev
+pip install aeneas
+# webrtcvad
+pip install webrtcvad
