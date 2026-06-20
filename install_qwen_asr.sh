31b203254aa65234196116ab9e2ecddbde8f3b57
fgwsz <3191000458@qq.com>
2026-06-20 19:32:05 +0800

    增加pip镜像源

--- install_qwen_asr.sh
+++ install_qwen_asr.sh
@@ -4,6 +4,7 @@ sudo apt install python3.10-venv
 python3 -m venv asr_env
 #进入asr_env环境
 source asr_env/bin/activate
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install --upgrade pip
 pip install qwen-asr
 python -c "from qwen_asr import Qwen3ASRModel; print('导入成功')"
--- install_qwen_tts.sh
+++ install_qwen_tts.sh
@@ -4,6 +4,7 @@ sudo apt install python3.10-venv
 python3 -m venv tts_env
 #进入tts_env环境
 source tts_env/bin/activate
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install --upgrade pip
 pip install qwen-tts
 sudo apt install -y sox   # 系统依赖
